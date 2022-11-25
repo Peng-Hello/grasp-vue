b125a52f1256abe9943d5fa934ece424e915a539
Peng <1936478563@qq.com>
2022-11-25 11:14:16 +0800

    test:添加编译器的 tokenize 函数,有不是字母的文本情况下的单元测试编写

--- src/test/compiler/tokenize.test.ts
+++ src/test/compiler/tokenize.test.ts
@@ -23,14 +23,29 @@ const useCase = {
             { type: "tagEnd", name: "div" },
         ],
     },
+    // 有不是字母的文本时
+    layer3: {
+        input: "<div>Hi!<p>Vue</p></div>",
+        output: [
+            { type: "tag", name: "div" },
+            { type: "text", content: "Hi!" },
+            { type: "tag", name: "p" },
+            { type: "text", content: "Vue" },
+            { type: "tagEnd", name: "p" },
+            { type: "tagEnd", name: "div" },
+        ],
+    },
 };
 
 describe("编译模块", () => {
-    const { layer1, layer2 } = useCase;
+    const { layer1, layer2, layer3 } = useCase;
     it("tokenize 函数测试: 一层情况", () => {
         expect(tokenize(layer1.input)).toStrictEqual(layer1.output);
     });
     it("tokenize 函数测试: 二层情况", () => {
         expect(tokenize(layer2.input)).toStrictEqual(layer2.output);
+    });
+    it("tokenize 函数测试: 有不是字母的文本时", () => {
+        expect(tokenize(layer3.input)).toStrictEqual(layer3.output);
     });
 });
