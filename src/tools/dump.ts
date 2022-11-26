07cf3252240b9119b45d2f100426c7df8c77eaf8
Peng <1936478563@qq.com>
2022-11-26 16:32:26 +0800

    feat:编写输出 AST 节点信息的辅助函数

---
+++ src/tools/dump.ts
@@ -0,0 +1,16 @@
+import { ParseItem } from "../compiler/type/parse.type";
+// 深度遍历模板 AST 并且输出节点信息
+function dump(node: ParseItem, indent: number = 0) {
+    const desc =
+        node.type === "node"
+            ? ""
+            : node.type === "Element"
+            ? node.tag
+            : node.content;
+    console.log(`${"-".repeat(indent)}${node.type} ${desc}`);
+    // 如果有子节点继续遍历
+    if (node.children) {
+        node.children.forEach((item) => dump(node, indent + 2));
+    }
+}
+export default dump;
