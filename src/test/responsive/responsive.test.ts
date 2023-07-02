5191cd9ae501cfa4f9334be35a549ce7a2f34fef
Peng <70511773+Peng-Hello@users.noreply.github.com>
2023-07-02 21:34:55 +0800

    feat: 原来的一些问题

--- src/test/responsive/responsive.test.ts
+++ src/test/responsive/responsive.test.ts
@@ -12,14 +12,25 @@ const data: {
 let global = "This is global variable";
 
 describe("响应性系统", () => {
-    it("可以自行册副作用函数", () => {
+    it("可以自行册副作用函数", async () => {
         const refData = ref(data);
 
         effect(() => {
+            console.log("effect run ...");
+
             global = refData.test;
         });
 
         refData.test = "Good!";
+
+        // 一个不存在的属性导致副作用函数重新执行了，这不是我们想要的。
+        // 发生这个情况就是因为我们桶结构与问题，不能使得属性建立明确关系
+        await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                refData.no = "no";
+                resolve(refData);
+            }, 1000);
+        });
 
         expect(global).toBe("Good!");
     });
